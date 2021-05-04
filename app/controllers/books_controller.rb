2983a1232bcd45bd2dda7be3b3d5d7cf8ab56905
seiya1911 <n.siy1911cb@gmail.com>
2021-05-04 08:38:01 +0000

    [Add]validation

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,33 +1,34 @@
 class BooksController < ApplicationController
   def index
-    @books=Book.all
-    @book=Book.new
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
-    @book=Book.find(params[:id])
+    @book = Book.find(params[:id])
   end
 
   def create
-    book=Book.new(book_params)
-     if book.save
-       redirect_to book_path(book.id), notice:'Book was successfully created.'
+    @book = Book.new(book_params)
+     if @book.save
+       redirect_to book_path(@book.id), notice:'Book was successfully created.'
      else
-      render :index
+       @books = Book.all
+       render:index
      end
   end
 
 
   def edit
-    @book=Book.find(params[:id])
+    @book = Book.find(params[:id])
   end
 
   def update
-    book=Book.find(params[:id])
-    if book.update(book_params)
-    redirect_to book_path(book),notice:'Book was seccessfully updated'
+    @book=Book.find(params[:id])
+    if @book.update(book_params)
+    redirect_to book_path(@book.id),notice:'Book was seccessfully updated'
     else
-      render :index
+      render:edit
     end
   end
 
